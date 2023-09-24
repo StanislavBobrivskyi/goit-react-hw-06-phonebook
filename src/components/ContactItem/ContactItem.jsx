120fef3432fef7d7a38907eaacdffa43f28b0380
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-09-24 15:30:21 +0200

    d

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -9,28 +9,26 @@ import 'react-toastify/dist/ReactToastify.css';
 export const ContactItem = ({ id, name, number }) => {
   const dispatch = useDispatch();
   return (
-    <>
-      <ListWrapper>
-        {name}: {number}
-        <DeleteBtn
-          type="button"
-          onClick={() => {
-            dispatch(deleteContact(id));
-            toast.success(`Contact "${name}" deleted successfully!`, {
-              position: 'top-center',
-              autoClose: 3000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              theme: 'colored',
-            });
-          }}
-        >
-          Delete
-        </DeleteBtn>
-      </ListWrapper>
-    </>
+    <ListWrapper>
+      {name}: {number}
+      <DeleteBtn
+        type="button"
+        onClick={() => {
+          dispatch(deleteContact(id));
+          toast.success(`Contact "${name}" deleted successfully!`, {
+            position: 'top-center',
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'colored',
+          });
+        }}
+      >
+        Delete
+      </DeleteBtn>
+    </ListWrapper>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from './redux/store';
-import { store } from './redux/store';
+
+import { store } from 'redux/store';
 import { App } from './components/App.jsx';
 import { GlobalStyle } from 'components/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={null}>
         <GlobalStyle />
         <App />
       </PersistGate>
