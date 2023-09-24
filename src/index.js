1e1c9ea4ce5a0f7a1f34efe5c5fe286b08f1fb46
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-09-24 15:10:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, stor } from 'redux/store';
+import { persistor, stor } from './redux/store';
 
 import { App } from 'components/App';
 import { GlobalStyle } from 'components/GlobalStyle';
