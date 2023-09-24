7f45965e032e2713dbd31beb566793f0a24d5467
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-09-24 15:55:08 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { App } from './components/App.jsx';
 import { GlobalStyle } from 'components/GlobalStyle';
 import store from 'redux/store.js';
-import { persistor } from 'redux/store';
+import { persistor } from 'redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
