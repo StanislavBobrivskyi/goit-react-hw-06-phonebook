937183390bd226ec03e58e96d9bfda12e53a2a9c
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-09-24 15:32:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-
+import { persistor } from 'redux/store';
 import { store } from 'redux/store';
 import { App } from './components/App.jsx';
 import { GlobalStyle } from 'components/GlobalStyle';
@@ -10,7 +10,7 @@ import { GlobalStyle } from 'components/GlobalStyle';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null}>
+      <PersistGate loading={null} persistor={persistor}>
         <GlobalStyle />
         <App />
       </PersistGate>
